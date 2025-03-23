f18d83b4cca60c5d3db6deb9b3d3a344bbb90221
bagasdprs <bagasdprs@gmail.com>
2025-03-23 20:12:52 +0700

    belum ada databasenya

---
+++ inc/footer.php
@@ -0,0 +1,5 @@
+<footer id="footer" class="footer fixed-bottom">
+  <div class="copyright">
+    &copy; Copyright <strong><span>Bagas D.</span></strong> All Rights Reserved
+  </div>
+</footer>
---
+++ inc/sidebar.php
@@ -0,0 +1,55 @@
+<aside id="sidebar" class="sidebar">
+  <ul class="sidebar-nav" id="sidebar-nav">
+
+    <li class="nav-item">
+      <a class="nav-link collapsed" href="index.php">
+        <i class="bi bi-grid"></i>
+        <span>Dashboard</span>
+      </a>
+    </li><!-- End Dashboard Nav -->
+    <li class="nav-item">
+      <a class="nav-link collapsed" href="resume.php">
+        <i class="bi bi-file-person-fill"></i>
+        <span>Resume</span>
+      </a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link collapsed" href="project.php">
+        <i class="bi bi-cast"></i>
+        <span>Project</span>
+      </a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link collapsed" href="services.php">
+        <i class="bi bi-tools"></i>
+        <span>Services</span>
+      </a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link collapsed" href="blog.php">
+        <i class="bi bi-newspaper"></i>
+        <span>Blog</span>
+      </a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link collapsed" href="contact.php">
+        <i class="bi bi-person-lines-fill"></i>
+        <span>Contact</span>
+      </a>
+    </li>
+
+    <li class="nav-item">
+      <a class="nav-link collapsed" href="skill.php">
+        <i class="bi bi-eyeglasses"></i>
+        <span>Skills</span>
+      </a>
+    </li>
+
+    <li class="nav-item">
+      <a class="nav-link collapsed" href="setting.php?sibedar=setting">
+        <i class="bi bi-gear-fill"></i>
+        <span>Settings</span>
+      </a>
+    </li>
+  </ul>
+</aside>
